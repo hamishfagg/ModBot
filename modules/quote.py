f6983c86189e54c9eeff5e7241adf0ed80b3cb61
MystX <mystx@chaostheory.co.nz>
2011-08-07 19:21:09 +1200

    Quote module ported.

--- modules/quote.py
+++ modules/quote.py
@@ -1,4 +1,5 @@
-import MySQLdb as mysql
+import sys
+from constants import *
 
 class Module():
 
@@ -6,7 +7,7 @@ class Module():
 	commands = {'q': 		'quote',
 				'quote':	'quote',
 				'rq':		'randQuote',
-				'randquote':'randQUote',
+				'randquote':'randQuote',
 				'nq':		'newQuote',
 				'newquote': 'newQuote'}
 	hooks =    {'loaded': 'loaded'}
@@ -31,7 +32,13 @@ class Module():
 		}
 	}
 
+	def __init__(self):
+		if 'twitter' in sys.modules:
+			self.depends.append('twitter')
+	
 	def loaded(self):
+		if 'twitter' not in sys.modules:
+			self.log.log(LOG_INFO, "Twitter module is not loaded.\n\t\t\t\tQuotes will not be tweeted unless the Twitter module is loaded and the Quote module is reloaded")
 		self.mysql.connect('quotes')
 	
 	def quote(self, user, channel, args):
@@ -39,36 +46,42 @@ class Module():
 			quote = self.mysql.find(fields=['quote'], order="created DESC", limit=1)[0]
 			self.main.msg(channel, quote[0], 10000)
 		else:
-			quotes = self.mysql.find(fields=['quote'], table="quotes WHERE UPPER(`quote`) LIKE UPPER(%s)" % " ".join(args), limit=6)
-			for quote in quotes:
-				self.main.msg(channel, self.main.colour + quote[0], MSGHACK)
-				if i == 4:
+			string = "%" + " ".join(args) + "%"
+			quotes = self.mysql.execute("SELECT * FROM quotes WHERE UPPER(`quote`) LIKE UPPER(%s) LIMIT 6",  (string,))
+			for i in range(5):
+				if i == len(quotes):
 					break
-			if len(results) > 5:
-				self.main.msg(channel, "%sExceeded maximum search results. Try a more specific search" % self.main.colour, MSGHACK)
+				self.main.msg(channel, self.main.colour + quotes[i][2], MSG_MAX)
+			if len(quotes) > 5:
+				self.main.msg(channel, "%sExceeded maximum search results. Try a more specific search" % self.main.colour, MSG_MAX)
+
+	def randQuote(self, user, channel, args):
+		quote = self.mysql.find(order="rand()", limit=1)
+		self.main.msg(channel, quote[0][2], MSG_MAX)
+
+	def newQuote(self, user, channel, args):
+		if user in self.main.admins:
+			self.mysql.insert(data={'user': user, 'quote': " ".join(args)})
+			self.main.msg(channel, "Quote was saved successfully", MSG_MAX)
+			
+			if self.main.modules.get('twitter', None) != None:
+				pass
 
 	def privmsg(self, user, channel, message):
 		if channel == self.main.nickname:
 			channel = user
 		
 		words = message.split(' ', 1)
-
-		if words[0] == '!rq' or words[0] == '!randquote':
-			#self.cur.execute("""SELECT * FROM quotes ORDER BY rand() LIMIT 1""")
-			#self.main.msg(channel, self.cur.fetchone()[2], MSGHACK)
-			pass
-		if words[0] == '!q' or words[0] == '!quote':
-			pass
 		
 		if words[0] == '!sq' or words[0] == '!nq' or words[0] == '!storequote' or words[0] == '!newquote':
 			print user
 			if user in self.main.admins:
 				try:
 					self.cur.execute("""INSERT INTO `quotes` (`user`, `quote`, `created`) VALUES (%s, %s, CURRENT_TIMESTAMP)""",  (user, words[1]))
-					self.main.msg(self.main.factory.channel, "%sQuote saved successfully." % self.main.colour, MSGHACK)
+					self.main.msg(self.main.factory.channel, "%sQuote saved successfully." % self.main.colour, MSG_MAX)
 					if self.main.modules.get('twitter', None) != None:
 						self.main.modules['twitter'].tweetQuote(words[1])
 				except:
-					self.main.msg(self.main.factory.channel, "%sAn error occured saving that quote: %s" % (self.main.colour, str(sys.exc_info()[1])) , MSGHACK)
+					self.main.msg(self.main.factory.channel, "%sAn error occured saving that quote: %s" % (self.main.colour, str(sys.exc_info()[1])) , MSG_MAX)
 			else:
-				self.main.msg(self.main.factory.channel, "%sYou're not an admin." % self.main.colour, MSGHACK)
+				self.main.msg(self.main.factory.channel, "%sYou're not an admin." % self.main.colour, MSG_MAX)
