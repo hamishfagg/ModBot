daa14b9ba17e18c1f91f02cd6bb3341855125acf
MystX <mystx@chaostheory.co.nz>
2011-08-07 14:49:23 +1200

    nope module ported

---
+++ modules/nope.py
@@ -0,0 +1,13 @@
+from constants import *
+
+class Module():
+	hooks = {'privmsg': 'privmsg'}
+
+	def privmsg(self, user, channel, msg):
+		if msg.startswith('!'):
+			msg = msg[1:]
+		if msg.lower().startswith('nope.avi'):
+			self.main.say(self.main.factory.channel, 'http://www.youtube.com/watch?v=gvdf5n-zI14', MSG_MAX)
+		elif msg.lower().startswith('brilliant!'):
+			self.main.say(self.main.factory.channel, 'http://www.youtube.com/watch?v=NBh895KdXAU', MSG_MAX)
+			
