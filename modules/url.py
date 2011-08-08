76a73757c7f1594163a58113e4fd7d5cd95b5c2e
MystX <mystx@chaostheory.co.nz>
2011-08-08 16:06:10 +1200

    Make URL module more helper friendly.

--- modules/url.py
+++ modules/url.py
@@ -6,7 +6,9 @@ class Module():
 	commands = {'shorten': 'shorten'}
 
 	def shorten(self, user, channel, args):
-		cmd = 'curl https://www.googleapis.com/urlshortener/v1/url -H "Content-Type: application/json" -d \'%s\'' % '{"longUrl": "%s"}' % args[0]
+		self.main.msg(channel, self.getShortUrl(args[0]), MSG_MAX)
+
+	def getShortUrl(self, url):
+		cmd = 'curl https://www.googleapis.com/urlshortener/v1/url -H "Content-Type: application/json" -d \'%s\'' % '{"longUrl": "%s"}' % url
 		output, err = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-		url = output[output.index('"id"'):].split('"')[3]
-		self.main.msg(channel, url, MSG_MAX)
+		return output[output.index('"id"'):].split('"')[3]
