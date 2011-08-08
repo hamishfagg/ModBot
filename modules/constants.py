4d049bbac9bd0b72a1c475450f87814f53a0f726
MystX <mystx@chaostheory.co.nz>
2011-08-08 21:07:12 +1200

    Forgot this file lol.

---
+++ modules/constants.py
@@ -0,0 +1,31 @@
+## HAX ##
+MSG_MAX = 1000
+
+## COLOURS ##
+COLOUR_WHITE 		= chr(3) + '0'
+COLOUR_BLACK 		= chr(3) + '1'
+COLOUR_BLUE 		= chr(3) + '2'
+COLOUR_GREEN 		= chr(3) + '3'
+COLOUR_RED 			= chr(3) + '4'
+COLOUR_BROWN 		= chr(3) + '5'
+COLOUR_PURPLE 		= chr(3) + '6'
+COLOUR_ORANGE 		= chr(3) + '7'
+COLOUR_YELLOW 		= chr(3) + '8'
+COLOUR_LIGHTGREEN 	= chr(3) + '9'
+COLOUR_TEAL 		= chr(3) + '10'
+COLOUR_CYAN 		= chr(3) + '11'
+COLOUR_LIGHTBLUE 	= chr(3) + '12'
+COLOUR_PINK 		= chr(3) + '13'
+COLOUR_GREY 		= chr(3) + '14'
+COLOUR_LIGHTGREY 	= chr(3) + '15'
+
+COLOUR_DEFAULT		= chr(15)
+COLOUR_BOLD			= chr(2)
+COLOUR_UNDERLINE	= chr(31)
+
+
+## LOGGING ##
+LOG_DEBUG   = 4
+LOG_INFO    = 3
+LOG_WARNING = 2
+LOG_ERROR   = 1
