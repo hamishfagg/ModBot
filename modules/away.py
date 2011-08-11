629d47b1ce2c87af115a49740b072231b103b392
MystX <hamish.fagg@gmail.com>
2011-08-11 15:30:18 +1200

    Fixed a small problem.

--- modules/away.py
+++ modules/away.py
@@ -23,14 +23,14 @@ class Module():
 				reason = 'None'
 		if self.reasons.get(user.lower(), None) == None:
 			self.reasons[user.lower()] = {'reason': reason, 'mentions': []}
-			self.main.say(self.main.factory.channel, "%s%s is now away. Reason: %s." % (COLOUR_DEFAULT, user, reason), MSG_MAX)
+			self.main.say(channel, "%s%s is now away. Reason: %s." % (COLOUR_DEFAULT, user, reason), MSG_MAX)
 		else:
 			if len(self.reasons[user.lower()]['mentions']) > 0:
 				self.main.msg(user, 'You were called for %s times while you were gone:' % len(self.reasons[user.lower()]['mentions']), MSG_MAX)
 				for line in self.reasons[user.lower()]['mentions']:
 					self.main.msg(user, line, MSG_MAX)
 			del self.reasons[user.lower()]
-			self.main.say(self.main.factory.channel, "%s%s is no longer away." % (COLOUR_DEFAULT, user), MSG_MAX)
+			self.main.say(channel, "%s%s is no longer away." % (COLOUR_DEFAULT, user), MSG_MAX)
 
 	def userLeft(self, user, channel):
 		if self.reasons.get(user.lower(), None) != None:
@@ -39,9 +39,9 @@ class Module():
 	def privmsg(self, user, channel, message):
 		words = message.split(' ', 1)
 
-		if self.reasons.get(user.lower(), None) != None:
+		if self.reasons.get(user.lower(), None) != None and not message.startswith('!away'):
 			self.main.notice(user, 'You are currently set as away.')
 
 		elif (words[0].endswith(':') or words[0].endswith(',')) and self.reasons.get(words[0][:-1].lower(), None) != None:
-			self.main.say(self.main.factory.channel, "%s%s is currently away. Reason: %s" % (COLOUR_DEFAULT, words[0][:-1], self.reasons.get(words[0][:-1].lower())['reason']), MSG_MAX)
+			self.main.say(channel, "%s%s is currently away. Reason: %s" % (COLOUR_DEFAULT, words[0][:-1], self.reasons.get(words[0][:-1].lower())['reason']), MSG_MAX)
 			self.reasons[words[0][:-1].lower()]['mentions'].append('<%s> %s' % (user, message))
