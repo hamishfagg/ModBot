19108755d145669a74f7b6a5c821c863e813d1ef
MystX <hamish.fagg@gmail.com>
2011-11-27 18:54:45 +1300

    Added help module and separate help files.

---
+++ modules/help/twitterhelp.py
@@ -0,0 +1,11 @@
+help = {
+	'desc': 'tweets things from chan to the ChaosTheory twitter account - ChaosTheoryServ.',
+	'commands': {
+		'tweet': {
+			'desc': 'Tweets the supplied message.',
+			'params': {
+				'message': 'String to tweet. Must be 140 characters or less.'
+			}
+		}
+	}
+}
---
+++ modules/help/urlhelp.py
@@ -0,0 +1,11 @@
+help = {
+	'desc': 'provides URL utilities.',
+	'commands': {
+		'shorten': {
+			'desc': 'Shortens a given URL using goo.gl.',
+			'params': {
+				'url': 'The URL you want shortened.'
+			}
+		}
+	}
+}
