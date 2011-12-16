6ab765164f616e68803bfbdc90d4a064f6a1aa01
RealityFork <mystx@chaostheory.co.nz>
2011-12-16 15:19:54 +1300

    Removed help dict.

--- modules/quote.py
+++ modules/quote.py
@@ -12,26 +12,6 @@ class Module():
                 'newquote': 'newQuote'}
     hooks =    {'loaded': 'loaded'}
 
-    help = {
-        'desc': 'searches, prints or adds new quotes. Tweets new quotes as they are added (if the Twitter module is loaded)',
-        'quote': {
-            'desc': 'Searches the quote database given a search term. If none is given, this command prints the latest quote.',
-            'params': {
-                'searchstring': 'String term to search for in the quote database (optional).'
-            }
-        },
-        'newquote': {
-            'desc': 'Adds a new quote to the database. Tweets the new quote given it is short enough and the Twitter module is loaded.',
-            'params': {
-                'quote': 'Quote to add to the database.'
-            }
-        },
-        'randquote': {
-            'desc': 'Prints a random quote.',
-            'params': {}
-        }
-    }
-
     def __init__(self):
         if 'twitter' in sys.modules:
             self.depends.append('twitter')
