e1bf424ef93765815edb3683ca01f4739e78cb95
RealityFork <hamish.fagg@gmail.com>
2012-01-10 20:39:07 +1300

    SHOULD be final multichan version.

--- modules/twitter.py
+++ modules/twitter.py
@@ -5,30 +5,25 @@ class Module():
 
     depends = ['logger']
     commands = {'tweet': 'tweet'}
-    help = {
-        'desc': 'tweets things from chan to the ChaosTheory twitter account - ChaosTheoryServ',
-        'tweet': {
-            'desc': 'Tweets the supplied message.',
-            'params': {
-                'message': 'String to tweet. Must be 140 characters or less.'
-            }
-        }
-    }
 
     def tweet(self, user, channel, args):
         tweet = " ".join(args)
         if user in self.main.channels[channel]['admins']:
             if len(tweet) > 140:
                 self.main.say(channel, "Tweet too long, please shorten it by %s characters." % (len(tweet) - 140), MSG_MAX)
-            else:   
-                if twitterhandler.tweet(tweet):
+            else:
+                ret = twitterhandler.tweet(tweet)
+                if ret == 1:
                     self.main.say(channel, "Sent tweet.", MSG_MAX)
                 else:
-                    self.main.say(channel, "Sending tweet failed. Check logs for details.", MSG_MAX)
+                    self.main.say(channel, ret, MSG_MAX)
+                    self.logger.log(LOG_ERROR, ret)
             
     def tweetQuote(self, quote):
         if len(quote) > 140:
             self.main.say(self.main.channel, "Couldn't tweet that quote - it's too long. You need to shorten it by %s characters." % (len(quote) - 140), MSG_MAX)
-        elif not twitterhandler.tweet(quote):
-            self.main.say(self.main.channel, "Sending quote tweet failed. Check logs for details.", MSG_MAX)
-
+        else:
+            ret = twitterhandler.tweet(quote)
+            if ret != 1:
+                self.main.say(self.channel, ret, MSG_MAX)
+                self.logger.log(LOG_ERROR, ret)
